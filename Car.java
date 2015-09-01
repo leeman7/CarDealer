ec0108bb4df04e249edfd2ea026ee3a864d3ac9f
Magnus Billups <magnus@magnusb.net>
2015-09-01 14:20:52 -0400

    create class outlines for project

---
+++ Car.java
@@ -0,0 +1,36 @@
+/*
+ * 
+ *
+*/
+
+public class Car{
+	
+	String VIN;
+	String name;
+	int year;
+	int mileage;
+	double price;
+
+	public Car(){
+	}
+
+	public void getVIN(){
+	
+	}
+
+	public void getName(){
+	
+	}
+
+	public void getYear(){
+	
+	}
+
+	public void getMiles(){
+	
+	}
+
+	public getPrice(){
+	
+	}
+}
